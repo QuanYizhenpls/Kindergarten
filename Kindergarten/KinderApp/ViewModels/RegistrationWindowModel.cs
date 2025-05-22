43c028ad002091fb72b5aef9f400aee5aaea0bd6
QuanYizhenpls <163442210+QuanYizhenpls@users.noreply.github.com>
2025-05-22 10:08:28 +0300

    w30

--- Kindergarten/KinderApp/ViewModels/RegistrationWindowModel.cs
+++ Kindergarten/KinderApp/ViewModels/RegistrationWindowModel.cs
@@ -73,14 +73,77 @@ namespace KinderApp.ViewModels
         private string login = string.Empty;
         private string password = string.Empty;
 
-        public string Firstname { get => firstname; set => Set(ref firstname, value, nameof(Firstname)); }
-        public string Lastname { get => lastname; set => Set(ref lastname, value, nameof(Lastname)); }
+        public string Firstname
+        {
+            get => firstname; set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    MessageBox.Show("Поле 'Имя' не может быть пустым.");
+                    return;
+                }
+                Set(ref firstname, value, nameof(Firstname));
+            }
+        }
+        public string Lastname { get => lastname; set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    MessageBox.Show("Поле 'Фамилия' не может быть пустым.");
+                    return;
+                }
+                Set(ref lastname, value, nameof(Lastname));
+            }
+        }
         public string Middlename { get => middlename; set => Set(ref middlename, value, nameof(Middlename)); }
-        public string Login { get => login; set => Set(ref login, value, nameof(Login)); }
-        public string Password { get => password; set => Set(ref password, value, nameof(Password)); }
+        public string Login { get => login; set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    MessageBox.Show("Поле 'Логин' не может быть пустым.");
+                    return;
+                }
+                Set(ref login, value, nameof(Login));
+            }
+        }
+        public string Password { get => password; set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    MessageBox.Show("Поле 'Пароль' не может быть пустым.");
+                    return;
+                }
+                Set(ref password, value, nameof(Password));
+            }
+        }
 
         public RelayCommand LoginCommand { get; }
         public RelayCommand RegisterCommand { get; }
         public RelayCommand CloseCommand { get; }
+
+        private bool ValidateData()
+        {
+            if (string.IsNullOrEmpty(Firstname))
+            {
+                MessageBox.Show("Поле 'Имя' не может быть пустым.");
+                return false;
+            }
+            if (string.IsNullOrEmpty(Lastname))
+            {
+                MessageBox.Show("Поле 'Фамилия' не может быть пустым.");
+                return false;
+            }
+            if (string.IsNullOrEmpty(Login))
+            {
+                MessageBox.Show("Поле 'Логин' не может быть пустым.");
+                return false;
+            }
+            if (string.IsNullOrEmpty(Password))
+            {
+                MessageBox.Show("Поле 'Пароль' не может быть пустым.");
+                return false;
+            }
+            return true;
+        }
     }
 }
